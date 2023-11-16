cd9b018946479cbdf82ff9b6cc1942a2ff18669f
Konstantin <57582571+konvod93@users.noreply.github.com>
2023-11-16 20:30:26 +0200

    Added customer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,32 @@
-import { customers } from '@/app/lib/placeholder-data';
-import CustomersTable from '@/app/ui/customers/table';
-import { Metadata } from 'next';
+import {fetchFilteredCustomers} from '@/app/lib/data'
+import CustomersTable from '@/app/ui/customers/table'
+import Search from '@/app/ui/search'
+import type {Metadata} from 'next'
 
 export const metadata: Metadata = {
-  title: 'Customers',
-};
+	title: 'Customers',
+}
 
-export default function Page() {   
-    <CustomersTable customers={[]} />
-  }
+export default async function Page({
+	searchParams,
+}: {
+	searchParams?: {
+		query?: string
+	}
+}) {
+	const query = searchParams?.query || ''
+
+	const customers = await fetchFilteredCustomers(query)
+
+	return (
+		<div className="w-full">
+			<div className="flex w-full items-center justify-between">
+				<h1 className="font-serif text-2xl">Customers</h1>
+			</div>
+			<div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+				<Search placeholder="Search customers..." />
+			</div>
+			<CustomersTable customers={customers} />
+		</div>
+	)
+}
